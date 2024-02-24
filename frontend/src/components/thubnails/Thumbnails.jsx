1ab0dddf96e80d84b93f16d59c1426f3cf7ec90a
CodeLordGh <babohkossi228@icloud.com>
2024-02-24 17:40:04 +0000

    css modules import updated

--- frontend/src/components/thubnails/Thumbnails.jsx
+++ frontend/src/components/thubnails/Thumbnails.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import classes from "./thumbnails.module.css"
-import StarRating from '../StarRating/StarRating'
-import Price from '../price/Price'
+import { Link } from "react-router-dom";
+import classes from "./Thumbnails.module.css";
+import StarRating from "../StarRating/StarRating";
+import Price from "../price/Price";
 
-export default function Thumbnails({foods}) {
+export default function Thumbnails({ foods }) {
   return (
     <ul className={classes.list}>
-      {foods.map(food => {
-        console.log(food.name)
+      {foods.map((food) => {
+        console.log(food.name);
         return (
           <li key={food.id}>
             <Link to={`/food/${food.id}`}>
@@ -17,35 +16,37 @@ export default function Thumbnails({foods}) {
                 src={`/foods/${food.imageUrl}`}
                 alt={food.name}
               />
-            <div className={classes.content}>
-              <div className={classes.name}>{food.name}</div>
-              <span className={`${classes.favorite} ${food.favorite ? "": classes.not}`}>❤</span>
-              <div className={classes.stars}>
-                <StarRating stars={food.stars} />
-              </div>
-              <div className={classes.product_item_footer}>
-                <div className={classes.origins}>
-                  {
-                    food.origins.map(origin => {
-                        <span key={origin}>
-                          {origin}
-                        </span>
-                    })
-                  }
+              <div className={classes.content}>
+                <div className={classes.name}>{food.name}</div>
+                <span
+                  className={`${classes.favorite} ${
+                    food.favorite ? "" : classes.not
+                  }`}
+                >
+                  ❤
+                </span>
+                <div className={classes.stars}>
+                  <StarRating stars={food.stars} />
                 </div>
-                <div className={classes.cook_time}>
-                  <span>⌚</span>
-                  {food.cookTime}
+                <div className={classes.product_item_footer}>
+                  <div className={classes.origins}>
+                    {food.origins.map((origin) => {
+                      <span key={origin}>{origin}</span>;
+                    })}
+                  </div>
+                  <div className={classes.cook_time}>
+                    <span>⌚</span>
+                    {food.cookTime}
+                  </div>
+                </div>
+                <div className={classes.price}>
+                  <Price price={food.price} />
                 </div>
               </div>
-              <div className={classes.price}>
-                <Price price={food.price} />
-              </div>
-            </div>
             </Link>
           </li>
-        )
+        );
       })}
     </ul>
-  )
+  );
 }
